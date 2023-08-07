1d92ecd9818b21581a9ba6d4073272114eae8dba
rohitbalshi31 <rohitbalshi11@gmail.com>
2023-08-08 02:27:48 +0530

    intial commit

---
+++ quora-testbook/README.md
@@ -0,0 +1,13 @@
+# Quora-Clone-Testbook
+
+This is the Clone of Quora made using react js
+. It has all the features like login, logout and profile page.
+
+## Getting Started
+1) git clone https://github.com/sagar2938/Quora-Clone-Testbook
+2) cd quora-clone-testbook
+3) npm install or yarn
+4) npm start
+5) open http://localhost:5173 in your browser
+
+    
---
+++ quora-testbook/src/data/Space.jsx
@@ -0,0 +1,40 @@
+let Space = [
+    
+    {
+        id:"1",
+        img:"https://lh3.googleusercontent.com/RIMTxCvr77RdtGt-xBH_5HekfvEDtbi4n1FursK2prK6QP2TzH5qMGzg27nq-y_mDz1cn-yCQZp1RirLhi-oJvH0xS76jmw8rqNNZiPPO4gR",
+        space:"Science",
+    },
+    {
+        id:"2",
+        img:"https://lh3.googleusercontent.com/8eBHO3m46xJwQVvdfFp6MEQwMxq0lAXzzb2eJG_wnUL2uGOr9DF6pRZ8cFAPb0BzzHAhmYGR0IFYZZJ9R91IzBiLxQYcTbcfkf59aGZyOZc",
+        space:"Technology",
+    },
+    {
+        id:"3",
+        img:"https://lh3.googleusercontent.com/w-Iz6-BImSuz9KbbItIf9ww2pNjOUXdXuyd6BKdFPDHFbtCkv1db9GgIhNRo4hdbKU3WMLWlGZuOSQCVx348roa-dX61rjLDRYKMkvqcZx5E",
+        space:"Music",
+    },
+    {
+        id:"4",
+        img:"https://lh3.googleusercontent.com/NoqTH3L53wfad8WnRUpSxbhaVi5bwtMeey6KLqWILBPB9En35dpqsfrh5q4KoA8JdVlRNoWBEt9RjUEB1VNYPYio5kAtBJxhTsAKCgJv8YrG",
+        space:"Politics",
+    },
+    {
+        id:"5",
+        img:"https://lh3.googleusercontent.com/h609GfCE_1f4T6eionNVDes84EQCnlpfFFno3sgf6rhfyuGpnsG1Z_VbQ0-FAyz1Q5tVuR2Xg9b7-w7z9VPH493KGAIMEAIR-yIgxJGL4SbH",
+        space:"Marketing",
+    },
+    {
+        id:"6",
+        img:"https://lh3.googleusercontent.com/zV6W5QrI062Ra6AVRs3YZWML-k1M4alWokQlvEijJfltfBd5_8ESP8GIhh8dG3vL1V5BzPaOwTx_FFTjqzfNL2ikDJbnXbgB9mzq99JGtpD_",
+        space:"Sports",
+    },
+    {
+        id:"7",
+        img:"https://lh3.googleusercontent.com/bEhE_uu5dBxUtG24-elqXoWqm32KZ6l_Yogbo5VuJnONn4mz3LX_qjCpgs2YrqSuO3AQy0Fqr0K1OjGQCNvFUXnCgjegsscle5w6RZXj5tlO",
+        space:"Mobile Technology",
+    },
+];
+
+export {Space};
